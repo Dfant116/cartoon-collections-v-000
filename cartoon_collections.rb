7f565586c2d27fce3a363124f9c1d16fde5fc161
Dalton Fant <daltonfant16@gmail.com>
2018-02-11 21:22:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,13 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0
+  if array.any? {|i| i.length > 4}
+    true
+  else
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
