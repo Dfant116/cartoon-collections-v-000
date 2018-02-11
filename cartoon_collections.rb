344188b0943fc7896c3c6abbb74a3cbde1f2b50a
Dalton Fant <daltonfant16@gmail.com>
2018-02-11 21:14:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map! do |i|
-    "#{i}!"
+  planeteer_calls.collect do |i|
+    "i.capitalize + !"
   end
 end
 
