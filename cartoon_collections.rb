0f78864302d97dfc5fb86aba68d37868ae151825
Dalton Fant <daltonfant16@gmail.com>
2018-02-11 21:33:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,6 +22,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |i|
+    cheese_types.include?(i)
+    
 end
